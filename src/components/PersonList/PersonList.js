4326f69ab49465cb680b90ebf4b6c60e0019ef07
Nurkyz-Chynybaeva <nurkyzchynybaeva29@gmail.com>
2021-03-13 20:52:26 +0600

    PersonList css

--- src/components/PersonList/PersonList.js
+++ src/components/PersonList/PersonList.js
@@ -1,11 +1,10 @@
-import Person from './components/Person/Person';
-import classes from './components/PersonList.module.css';
-
-const PersonList = ({pesons}) => {
-  return(
-    <div className = {classes.PersonList}>
-      {persons.map(person => <Person {...person}/>)}
-    </div>
-  )
+import Person from "../Person/Person";
+import classes from "./PersonList.module.css";
+const PersonList = ({ persons }) => {
+    return (
+        <div className={classes.PersonList}>
+            {persons.map(person => <Person {...person} />)}
+        </div>
+    );
 }
 export default PersonList;
