fda4de7ed7fecfb04d77aee6b483c12c673e3898
Nurkyz-Chynybaeva <nurkyzchynybaeva29@gmail.com>
2021-03-13 22:06:43 +0600

    filtrated the Person list

--- src/components/PersonList/PersonList.js
+++ src/components/PersonList/PersonList.js
@@ -3,7 +3,7 @@ import classes from "./PersonList.module.css";
 const PersonList = ({ persons }) => {
     return (
         <div className={classes.PersonList}>
-            {persons.map(person => <Person {...person} />)}
+            {persons.filter(item => item.role == 'Student') .map(person => <Person {...person} />)}
         </div>
     );
 }
